82df7b07dc88afd1a04b3a6d6b2342e5207555fc
mboumlik <mboumlik@e2r13p8.1337.ma>
2024-01-08 16:24:10 +0100

    gogogogogo last push

--- get_next_line.h
+++ get_next_line.h
@@ -13,11 +13,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <fcntl.h>
 # include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-#include <fcntl.h>
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 12
 # endif
--- main2.c
+++
@@ -1,7 +0,0 @@
-#include "get_next_line.h"
-int main()
-{
-    char *p = get_next_line(0);
-    printf("%s",p);
-    return 0;
-}
